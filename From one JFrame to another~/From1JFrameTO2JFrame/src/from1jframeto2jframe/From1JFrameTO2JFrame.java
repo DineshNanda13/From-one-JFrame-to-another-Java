12f96319d04dc01e0d7f4a264157d04eeb315f5f
dineshnanda13 <dannynanda76@gmail.com>
2018-11-01 20:30:56 -0400

    Switch b/w JFrames

---
+++ From one JFrame to another~/From1JFrameTO2JFrame/src/from1jframeto2jframe/From1JFrameTO2JFrame.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package from1jframeto2jframe;
+
+/**
+ *
+ * @author SONY INDIA
+ */
+public class From1JFrameTO2JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame nf =new NewJFrame();
+        nf.setVisible(true);
+    }
+    
+}
